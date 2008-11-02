9e7c8f4ce8dffdb0d5788368e8950fbea8a4567f
Fabian Bergstrom <sempetmer@gmail.com>
2008-11-02 12:09:38 +0100

    Added transpose/1, and transpose/2, to transpose a board.

--- board.erl
+++ board.erl
@@ -15,3 +15,13 @@ col(0, Column) -> Column;
 col(W, Column) ->
     Tokens = [r, g, b],
     col(W - 1, [lists:nth(random:uniform(length(Tokens)), Tokens) | Column]).
+
+%% Transpose a board.
+transpose(Columns) -> % add accumulator and token
+    transpose([token | Columns], []).
+transpose([token,[] | _], Rows) -> % base case, we're done
+    lists:reverse(lists:map(fun lists:reverse/1, Rows));
+transpose([token, [Head | Tail] | Columns], Rows) -> % create a new column
+    transpose(Columns ++ [token | [Tail]], [[Head] | Rows]);
+transpose([[Head | Tail] | Columns], [Row | Rows]) -> % keep on truckin'
+    transpose(Columns ++ [Tail], [[Head | Row] | Rows]).
