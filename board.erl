e91602ab1589a6a75bd3e985bdbfc9ac227d6eba
Fabian Bergstrom <sempetmer@gmail.com>
2008-11-02 12:07:31 +0100

    Added new, cols, and col, to create boards.

--- board.erl
+++ board.erl
@@ -2,3 +2,16 @@
 -author('sempetmer@gmail.com').
 
 -compile(export_all).
+
+%% Generate a random board W by H
+new(W, H) ->
+    cols(W, H, []).
+cols(_W, 0, Columns) -> Columns;
+cols(W, H, Columns) ->
+    cols(W, H - 1, [col(W) | Columns]).
+col(W) ->
+    col(W, []).
+col(0, Column) -> Column;
+col(W, Column) ->
+    Tokens = [r, g, b],
+    col(W - 1, [lists:nth(random:uniform(length(Tokens)), Tokens) | Column]).
